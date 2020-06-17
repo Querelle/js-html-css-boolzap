c836c14ba4d1ff987c3fb5038a25d0470b26460a
Andrea De Brest <persona.qdb@gmx.com>
2020-06-17 17:18:38 +0200

    inserita delay risposta e funzione di ricerca

--- js/script.js
+++ js/script.js
@@ -1,17 +1,80 @@
 $(document).ready(function() {
+
+//  === invia messaggio al click
   $('#send').on('click', function() {
+    var textUser = $('#input-form').val();
+
     if($('#input-form').val() != "") {
-      var textUser = $('#input-form').val();
-      var dt = new Date();
-      var time = dt.getHours() + ":" + dt.getMinutes();
-      $('.chat-windows-template .io .timestamp').text(time);
-      $('.chat-windows-template .io .text').text(textUser);
-      $('.chat-windows-template .io').clone().appendTo('.sent-messages');
+      sendMessage(textUser, '.io');
+      $('.chat-windows').scrollTop($('.chat-windows').height());
+      setTimeout(function() {
+        sendMessage('ok', '.contatto')
+        $('.chat-windows').scrollTop($('.chat-windows').height());
+      }, 2000)
 
-      $('#input-form').val("");
-
-      var dt = new Date();
-      var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
     }
+
   })
+
+//  === invia messaggio premendo enter
+  $('#input-form').on('keydown', function(event) {
+
+    if(event.which == 13) {
+      var textUser = $('#input-form').val();
+
+      if($('#input-form').val() != "") {
+        sendMessage(textUser, '.io');
+        $('.chat-windows').scrollTop($('.chat-windows').height());
+        setTimeout(function() {
+          sendMessage('ok', '.contatto')
+          $('.chat-windows').scrollTop($('.chat-windows').height());
+        }, 2000)
+      }
+    }
+
+  })
+
+//  === searchbar
+  $('.search-input').on('keyup', function(event) {
+    var value = $('.search-input').val().toUpperCase();
+
+    $('.chat-list .name').each(function() {
+      if($(this).text().toUpperCase().includes(value)) {
+        $(this).parents('li').show();
+      } else {
+        $(this).parents('li').hide();
+      }
+    })
+
+
+  });
+
+//  === Funzioni
+
+//  === genera timestamp
+  function getTimestamp () {
+    var dt = new Date();
+    var time;
+    if( dt.getMinutes() < 10 ) {
+      time = dt.getHours() + ":" + 0 + dt.getMinutes();
+    } else {
+      time = dt.getHours() + ":" + dt.getMinutes();
+    }
+
+    return time;
+  }
+
+//  === invia messaggio
+  function sendMessage(text, sender) {
+    var time = getTimestamp();
+    var path = '.template ' + sender;
+
+    $('.template').find('.timestamp').text(time);
+    $('.template').find('.text').text(text);
+
+    $(path).clone().appendTo('.sent-messages');
+
+    $('#input-form').val("");
+  }
+
 });
